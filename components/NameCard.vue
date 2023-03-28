59ae6e58b2edf9ef3794dd64a1b04ddd3ff56dd1
Side-26 <mr72302@gmail>
2023-03-28 22:10:44 +0430

    fix build a component for found baby name

---
+++ components/NameCard.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const emit = defineEmits<{
+    (e: 'removeBaby', id: number): void;
+}>();
+// const emit = defineEmits(['removeBaby']);
+interface props {
+    name: string;
+    id: number;
+}
+const props = defineProps<props>();
+const removeCard = (id: number) => {
+    emit('removeBaby', id);
+    // alert(id);
+};
+</script>
+<template>
+    <div class="p-3 flex-grow flex-wrap">
+        <div class="bg-cyan-600 text-white p-3 rounded-xl">
+            <button
+                class="text-right w-4 ml-auto block mb-3"
+                @click="removeCard(id)"
+            >
+                x
+            </button>
+            <div class="text-center font-semibold pb-4">
+                {{ name }}
+            </div>
+        </div>
+    </div>
+</template>
